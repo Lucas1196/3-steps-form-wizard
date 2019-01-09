d6da10e674eb7549bdd76d6d1f31b969b91cf21c
Lucas1196 <45823230+Lucas1196@users.noreply.github.com>
2019-01-09 16:21:54 +0200

    Update form.js

--- form.js
+++ form.js
@@ -118,6 +118,7 @@ function validateForm() {
     var x, y, i, z, valid = true;
     x = document.getElementsByClassName("form");
     y = x[currentPage].getElementsByTagName("input");
+    z = document.getElementsByTagName("select");
     // A loop that checks every input field in the current tab:
     for (i = 0; i < y.length; i++) {
         // If a field is empty...
@@ -132,6 +133,14 @@ function validateForm() {
             valid = false;
         }
     }
+    
+    // if ( $('.select').val() == false ) {
+    //     alert("i am ok");
+    // }
+    // else {
+    //     alert("i am not ok");
+    // }
+
     // If the valid status is true, mark the step as finished and valid:
     if (valid) {
         document.getElementsByClassName("step")[currentPage].className += " finish";
